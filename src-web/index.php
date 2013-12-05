843f0f90a9e38b86c9d8a22f8c74901d2573b8ba
Caitlin Dagg <caitlin@lesfragnes.com>
2013-12-05 23:50:45 +0100

    css updated

--- src-web/index.php
+++ src-web/index.php
@@ -1,11 +1,12 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <html>
 <head>
 	<meta charset="utf-8" />
-	<title>Intuition</title>
+	<title>intuition.</title>
 	<link rel="stylesheet" media="only screen and (max-width:1024px)" href="style/mobile.css">
 	<link rel="stylesheet" media="only screen and (min-width:1024px)" href="style/desktop.css">
-	<link href='http://fonts.googleapis.com/css?family=Cabin+Sketch|Amatic+SC:400,700|Cuprum:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="fonts\font-awesome-4.0.3\css\font-awesome.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Raleway:800' rel='stylesheet' type='text/css'>
 	<!-- <link rel="icon" type="image/png" href="/images/favicon.png" /> --> 
 </head>
 <body>
@@ -15,9 +16,9 @@
 				calme
 			</div>
 			<div id="choices">
-				<div class="button yes">OUI</div>
-				<div class="button unsure">?</div>
-				<div class="button no">NON</div>
+				<div class="button yes"><i class="fa fa-check fa-fw"></i></div>
+				<div class="button unsure"><i class="fa fa-arrow-right fa-fw"></i></div>
+				<div class="button no"><i class="fa fa-times fa-fw"></i> </div>
 			</div>
 		</div>
 	</div>
