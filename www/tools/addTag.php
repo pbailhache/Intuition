22a5273ae80626febd56fd16b3696941b193f9ad
krakenRose <orain.juliette@gmail.com>
2013-12-06 02:02:20 +0100

    Outil ajout de tag

---
+++ www/tools/addTag.php
@@ -0,0 +1,6 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript" src="../scripts/addTag.js"></script> 
+
+<p>name:<input type="text" id="name"/></p>
+<p>color:<input type="text" id="color"/> (format 123456)</p>
+<div id="envoyer" style="border:1px black solid; width:100px ">Envoyer</div>
